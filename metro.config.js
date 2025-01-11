8d87a91f4e9f8a773cffbb25a81caa39339417fb
Dmitriy2893 <dmiriy2893@gmail.com>
2025-01-11 13:54:49 +0300

    init app

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,19 @@
 const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
 
+const defaultConfig = getDefaultConfig(__dirname);
+const { assetExts, sourceExts } = defaultConfig.resolver;
 /**
  * Metro configuration
  * https://reactnative.dev/docs/metro
  *
  * @type {import('metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+  resolver: {
+    assetExts: assetExts.filter(ext => ext !== 'svg'),
+    sourceExts: [...sourceExts, 'svg'],
+    resolverMainFields: ['sbmodern', 'react-native', 'browser', 'main'],
+  },
+};
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+module.exports = mergeConfig(defaultConfig, config);
